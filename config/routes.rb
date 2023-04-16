5d777da12e133bb2caf76b2896d71a14f0c992c6
EC2 Default User <ec2-user@ip-172-31-6-65.ap-northeast-1.compute.internal>
2023-04-16 08:07:24 +0000

    regular updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  #deviseのモデル作成に伴う、ルーティングの自動作成
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #「/」でトップページを表示できるようにする
+  root to: 'homes#top'
+  
 end
